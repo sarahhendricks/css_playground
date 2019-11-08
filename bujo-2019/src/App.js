c388ef3463dd6e868c7558726e3ae2148ecca326
Sarah Hendricks <slynnhendricks@gmail.com>
2019-11-07 20:17:29 -0500

    Creating new app
    Made basic React app that utilizes personal Pinterest boards. These
    were inspiration boards for my bullet journal this year. Need to learn
    the API for getting just the pins from the boards rather than using
    pre-made widgets.

---
+++ bujo-2019/src/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="https://www.pinterest.com/daisyinaglass/january-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+      <a href="https://www.pinterest.com/daisyinaglass/february-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+      <a href="https://www.pinterest.com/daisyinaglass/march-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+      <a href="https://www.pinterest.com/daisyinaglass/april-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+        <a href="https://www.pinterest.com/daisyinaglass/may-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+<a href="https://www.pinterest.com/daisyinaglass/june-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+<a href="https://www.pinterest.com/daisyinaglass/july-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+<a href="https://www.pinterest.com/daisyinaglass/august-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+<a href="https://www.pinterest.com/daisyinaglass/september-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+<a href="https://www.pinterest.com/daisyinaglass/october-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+<a href="https://www.pinterest.com/daisyinaglass/november-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+<a href="https://www.pinterest.com/daisyinaglass/december-inspo/"
+   data-pin-do="embedBoard"
+   data-pin-board-width="1200"
+   data-pin-scale-height="3025"
+   data-pin-scale-width="92">
+</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
